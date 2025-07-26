6a7b53e3cf1d0bc8dc0dd25766f9ded749ef6c87
Monal <mob.jupical@gmail.com>
2025-07-26 20:07:24 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Monal Bhuva",
+    author_email="monalbhuva727@gmail.com",
+    packages=find_packages()
+)
